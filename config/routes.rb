27612ee74aab0de97b390e2e6af57d643b718fa5
Juan Sanchez <juansanchez3096@sbcglobal.net>
2016-07-15 15:12:11 -0700

    added React support and basic User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Rails.application.routes.draw do
-  
-  match '/help',    to: 'static_pages#help',     via: 'get'
-  match '/about',   to: 'static_pages#about',    via: 'get'
-  match '/contact', to: 'static_pages#contact',  via: 'get'
+  get 'users/new'
+
+  root 'static_pages#home'
+  get '/help',    to: 'static_pages#help'
+  get '/about',   to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup',  to: 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'static_pages#home'
+  # root
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
