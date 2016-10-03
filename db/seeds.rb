296865c68ac5e5c7d3372b1a76afabc71d09916e
Juan Sanchez <juansanchez3096@sbcglobal.net>
2016-10-02 17:32:44 -0700

    added default exercises to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,3 +30,11 @@ users = User.order(:created_at).take(6)
 50.times do
   users.each { |user| user.workouts.create!(name: "Test Workout") }
 end
+
+Exercise.create!(name: "Dumbbell Skullcrushers", description: "Lay flat on a bench with an EZ curl bar. Position the bar in a bench press position and proceed to bring the bar down to your forehead. Lift the bar up and repeat this movement.")
+Exercise.create!(name: "Flat Bench Press", description: "Lay flat on a bench press. Position the barbell around the center of your chest. Proceed to bring the bar down until it touches your chest and then bring it up and repeat this movement.")
+Exercise.create!(name: "Barbell Back Squat", description: "Place the barbell along your traps muscles. Squat down to desired depth and bring the barbell up and repeat this movement.")
+Exercise.create!(name: "Close Grip EZ Bar Curls", description: "Hold an EZ bar with a close grip. Curl the bar up while keeping your arms and shoulders from swinging up. Repeat this movement.")
+Exercise.create!(name: "Dumbbell Shoulder Press", description: "Bring dumbells up to shoulder height. Press up and bring the weights back to shoulder height. Repeat this movement.")
+Exercise.create!(name: "Lat Pulldown", description: "In a seated upright position, pull the bar down to around chest height and bring it up. Repeat this movement.")
+
